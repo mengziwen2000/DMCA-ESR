b35f4a10f07cc2240930b7e9719256f74563e29f
mengziwen <mzw1207154476@gmail.com>
2023-05-07 18:37:20 +0800

    update my learning process 2023/5/7

---
+++ .ipynb_checkpoints/mymax-checkpoint.m
@@ -0,0 +1,17 @@
+function max = mymax(n1, n2, n3, n4, n5)
+
+%此函数用于计算
+%输入五个数字
+max =  n1;
+if(n2 > max)
+   max = n2;
+end
+if(n3 > max)
+   max = n3;
+end
+if(n4 > max)
+   max = n4;
+end
+if(n5 > max)
+   max = n5;
+end
